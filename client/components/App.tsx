fcf89818c8aad060a4a3cfff8fcedc4154a5b3a9
malyz <mattuy001@gmail.com>
2020-06-25 20:24:37 +1200

    added sample content for Nav component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 
+import Nav from './Nav'
 import Landing from './Landing'
 
 const App = () => {
   return (
     <HashRouter>
-      <Route exact path='/' component={Landing} />
+      <Nav id={1} name="Sample" />
+      <Route exact path="/" component={Landing} />
     </HashRouter>
   )
 }
