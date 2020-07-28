1216c1ad91f7b830d893369ff5cbd00cdd8e27c9
malyz <mattuy001@gmail.com>
2020-07-28 16:18:31 +1200

    NEW: added sequelize config

---
+++ server/db/config.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+const pool = {
+  max: 5,
+  min: 0,
+  acquire: 30000,
+  idle: 10000
+};
+
+const sequelize = new Sequelize(
+  process.env.PG_DB,
+  process.env.PG_USER,
+  process.env.PG_PASS,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool
+  }
+);
+
+module.exports = () =>
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connected to Database');
+    })
+    .catch((err) => {
+      console.log('Unable to connect to the Database', err);
+    });
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-require('dotenv').config()
-const path = require('path')
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config();
+require('./db/config')();
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
 
-const server = express()
+const server = express();
 
-server.use(cors())
-server.use(express.urlencoded({ extended: false }))
-server.use(express.json())
-server.use(express.static(path.resolve('server', 'public')))
+server.use(cors());
+server.use(express.urlencoded({ extended: false }));
+server.use(express.json());
+server.use(express.static(path.resolve('server', 'public')));
 
-module.exports = server
+module.exports = server;
