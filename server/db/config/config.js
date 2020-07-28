4d6deba6e6c74ac280c15a0625536a1c6e496d9b
malyz <mattuy001@gmail.com>
2020-07-28 18:56:59 +1200

    UPDATE: moved .env file inside db

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.PG_USER,
     password: process.env.PG_PASS,
     database: process.env.PG_DB,
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'postgres'
   },
   test: {
     username: 'root',
     password: null,
-    database: 'database_test',
+    database: '_test',
     host: '127.0.0.1',
     dialect: 'mysql'
   },
