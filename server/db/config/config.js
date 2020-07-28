fdf560db8193a031c147cb36820944f558a99f75
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-28 20:45:22 +1200

    make changes

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   'development': {
     'username': process.env.PG_USER,
     'password': process.env.PG_PASS,
     'database': process.env.PG_DB,
     'host': '127.0.0.1',
-    'dialect': 'mysql'
+    'dialect': 'postgres'
   },
   'test': {
     'username': 'root',
