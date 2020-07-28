a62b4ddd15ee66dec59075a76e4b7feb55058ca4
malyz <mattuy001@gmail.com>
2020-07-28 22:56:38 +1200

    UPDATE: changed production config

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    database: process.env.DATABASE_URL,
+    dialect: 'postgres'
   }
 };
