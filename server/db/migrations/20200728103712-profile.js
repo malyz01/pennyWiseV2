9c7a1d594325c3a0866cebba9a387fe7c5c94813
malyz <mattuy001@gmail.com>
2020-07-29 23:02:17 +1200

    UPDATE: refactored profile to have id

--- server/db/migrations/20200728103712-profile.js
+++ server/db/migrations/20200728103712-profile.js
@@ -1,33 +1,35 @@
-'use strict';
-const { v4: uuid } = require('uuid');
-
-const getUUID = () => uuid();
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('profiles', {
+    await queryInterface.createTable("profiles", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
       userId: {
         allowNull: false,
         type: Sequelize.UUID,
         references: {
-          model: 'users',
-          key: 'id'
-        }
+          model: "users",
+          key: "id",
+        },
       },
       fullName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       avatar: {
         type: Sequelize.STRING,
         defaultValue:
-          'https://cdn2.iconfinder.com/data/icons/web-mobile-2-1/64/user_avatar_admin_web_mobile_business_office-512.png'
+          "https://cdn2.iconfinder.com/data/icons/web-mobile-2-1/64/user_avatar_admin_web_mobile_business_office-512.png",
       },
       createdAt: Sequelize.DATE,
-      updatedAt: Sequelize.DATE
+      updatedAt: Sequelize.DATE,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('profiles');
-  }
+    await queryInterface.dropTable("profiles");
+  },
 };
--- server/db/seeders/20200728095507-profile.js
+++ server/db/seeders/20200728095507-profile.js
@@ -1,30 +1,34 @@
-'use strict';
+"use strict";
+const { v4: uuid } = require("uuid");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('profiles', [
+    await queryInterface.bulkInsert("profiles", [
       {
-        userId: 'de65d050-4e0e-4a79-8063-74e7b9e53dd4',
-        fullName: 'John Doe',
+        id: uuid(),
+        userId: "de65d050-4e0e-4a79-8063-74e7b9e53dd4",
+        fullName: "John Doe",
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        userId: '7b85a36f-abd4-4a76-9860-bd88670ef1f0',
-        fullName: 'Jane Doe',
+        id: uuid(),
+        userId: "7b85a36f-abd4-4a76-9860-bd88670ef1f0",
+        fullName: "Jane Doe",
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        userId: '8d96e758-08e6-4d85-adbb-2f5ba8308b85',
-        fullName: 'GI Joe',
+        id: uuid(),
+        userId: "8d96e758-08e6-4d85-adbb-2f5ba8308b85",
+        fullName: "GI Joe",
         createdAt: new Date(),
-        updatedAt: new Date()
-      }
+        updatedAt: new Date(),
+      },
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('profiles', null, {});
-  }
+    return queryInterface.bulkDelete("profiles", null, {});
+  },
 };
