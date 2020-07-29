1d880d7d8ec42d87505dad3aa23c802d5c0d32a7
malyz <mattuy001@gmail.com>
2020-07-29 16:12:36 +1200

    UPDATE: added createUser function

--- server/db/controller/users.js
+++ server/db/controller/users.js
@@ -1,14 +1,19 @@
-const { sequelize, Sequelize } = require("../models");
-const Users = require("../models/user")(sequelize, Sequelize.DataTypes);
+const { sequelize, Sequelize } = require('../models');
+const Users = require('../models/user')(sequelize, Sequelize.DataTypes);
+
+function createUser(data) {
+  return Users.create(data);
+}
 
 function getUser(id) {
   return Users.findOne({
     where: {
-      id,
-    },
+      id
+    }
   });
 }
 
 module.exports = {
-  getUser,
+  createUser,
+  getUser
 };
