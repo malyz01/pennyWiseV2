c992a10777d5d043d335760e9ec805bc49d1bf72
malyz <mattuy001@gmail.com>
2020-07-29 20:10:37 +1200

    UPDATE: added association one to one User and Profile

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,21 +3,15 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       this.hasOne(models.Profile),
         {
           foreignKey: 'userId',
-          as: 'profile',
           onDelete: 'CASCADE'
         };
     }
   }
+
   User.init(
     {
       id: {
