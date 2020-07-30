14e85021831d7643d1091a3c776dd5ea244e9faf
malyz <mattuy001@gmail.com>
2020-07-30 19:44:59 +1200

    UPDATE: refactor user resolver to make code smaller

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,9 +1,7 @@
 const { fetchAllUser } = require('./db/controller/users');
 
-const resolvers = {
+module.exports = {
   Query: {
     users: async (root, arg, context, info) => await fetchAllUser()
   }
 };
-
-module.exports = resolvers;
