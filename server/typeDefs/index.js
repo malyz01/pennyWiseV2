fd5f735d9ff02c09e2abbdce1c498e3f91fb1687
malyz <mattuy001@gmail.com>
2020-07-30 19:34:06 +1200

    UPDATED: added root in index typedef

--- server/typeDefs/index.js
+++ server/typeDefs/index.js
@@ -1,18 +1,4 @@
-const { gql } = require('apollo-server-express');
+const root = require('./root');
 const user = require('./user');
 
-const root = gql`
-  type Query {
-    _: String
-  }
-
-  type Mutation {
-    _: String
-  }
-
-  type Subscription {
-    _: String
-  }
-`;
-
-module.exports = [user];
+module.exports = [root, user];
