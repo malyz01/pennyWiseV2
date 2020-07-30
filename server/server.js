ab2ce9f227401fa31b59737876ac3562464cf82c
malyz <mattuy001@gmail.com>
2020-07-30 14:15:56 +1200

    NEW: added eslintrc

--- server/server.js
+++ server/server.js
@@ -3,50 +3,10 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const server = express();
-const User = require('./db/controller/users');
-
-// SAMPLE GRAPHQL
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
-
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton'
-  }
-];
-
-// The GraphQL schema in string form
-const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
-`;
-
-// The resolvers
-const resolvers = {
-  Query: { books: () => books }
-};
-
-// Put together a schema
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
-});
 
 server.use(cors());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use(express.static(path.resolve('server', 'public')));
-
-server.get('/ss', async (req, res) => {
-  res.json(await User.fetchAllUser());
-});
-// console.log(typeof graphqlExpress);
-// server.use('/graphql', graphqlExpress({ schema }));
-// server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 module.exports = server;
