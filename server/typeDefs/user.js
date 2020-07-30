94a1febc00816436147ba4f29b5b3e4cb82c6536
malyz <mattuy001@gmail.com>
2020-07-30 19:15:19 +1200

    UPDATE: made user typedef modular by adding extend

--- server/typeDefs/user.js
+++ server/typeDefs/user.js
@@ -1,7 +1,11 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
-  type User {
+  type Query {
+    users: [User]
+  }
+
+  extend type User {
     id: String
     email: String
     password: String
@@ -9,7 +13,7 @@ module.exports = gql`
     updatedAt: String
   }
 
-  type Query {
-    users: [User]
+  extend type Mutation {
+
   }
 `;
