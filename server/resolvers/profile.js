bb9faedbcadcb5c37982882caf553150a5e97e45
malyz <mattuy001@gmail.com>
2020-07-31 13:40:53 +1200

    UPDATE: added profile in index.js resolvers

--- server/resolvers/profile.js
+++ server/resolvers/profile.js
@@ -1,10 +1,10 @@
-const u = require('../db/controller/users');
+const p = require('../db/controller/profile');
 
 module.exports = {
   Query: {
-    profiles: (root, arg, context, info) => u.fetchAllUser(),
+    profiles: (root, arg, context, info) => p.fetchAllUserProfile(),
     profile: (root, arg, context, info) => {
-      return u.getUser(arg.id);
+      return p.getUserProfile(arg.id);
     }
   }
 };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
-
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
