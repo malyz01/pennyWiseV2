ba986f95543e586a6c63b858b4e45e259047b49b
malyz <mattuy001@gmail.com>
2020-07-31 12:44:48 +1200

    REMOVED: graphql-tools package and express.json usage

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const { ApolloServer } = require('apollo-server-express');
+
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-const { ApolloServer } = require('apollo-server-express');
 
 const IN_PROD = process.env.NODE_ENV === 'production';
 
@@ -12,7 +13,6 @@ const apollo = new ApolloServer({ typeDefs, resolvers, playground: !IN_PROD });
 const server = express();
 
 server.use(cors());
-server.use(express.json());
 server.use(express.static(path.resolve('server', 'public')));
 
 apollo.applyMiddleware({ app: server });
