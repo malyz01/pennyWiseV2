e61f2cb4d2f84f03863e47562d199217a9fbc928
malyz <mattuy001@gmail.com>
2020-08-03 22:21:24 +1200

    NEW: package react-swipeable-views

--- client/components/Landing/index.tsx
+++ client/components/Landing/index.tsx
@@ -3,6 +3,7 @@ import { connect, ConnectedProps } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 
+import Carousel from './Carousel';
 import * as users from '../../store/actions/users';
 
 const Landing = (props: IProps) => {
@@ -14,6 +15,7 @@ const Landing = (props: IProps) => {
     <div>
       <h1 style={{ textAlign: 'center' }}>Landing Page</h1>
       <Container>
+        <Carousel />
         {!!props.users.length &&
           props.users.map((u, i) => <div key={i}>{u.email}</div>)}
       </Container>
