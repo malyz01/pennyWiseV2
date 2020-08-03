029f96c7a86d6aa5691f10bb2aefc3678176d936
malyz <mattuy001@gmail.com>
2020-08-03 19:16:46 +1200

    UPDATE: added font boogaloo and ubuntu

--- client/Theme.ts
+++ client/Theme.ts
@@ -1,6 +1,15 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Ubuntu',
+    h1: {
+      fontFamily: 'Boogaloo'
+    },
+    h2: {
+      fontFamily: 'Boogaloo'
+    }
+  },
   palette: {
     primary: {
       main: '#00A6A4'
@@ -8,9 +17,6 @@ const theme = createMuiTheme({
     secondary: {
       main: '#d1a435'
     }
-  },
-  typography: {
-    //  allVariant
   }
 });
 
