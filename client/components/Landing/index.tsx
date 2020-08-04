0c03e1d958ae0bd9e3c7d2e2aa3882f6b12abb07
malyz <mattuy001@gmail.com>
2020-08-04 21:25:55 +1200

    UPDATE: clean up Landing component

--- client/components/Landing/index.tsx
+++ client/components/Landing/index.tsx
@@ -1,17 +1,12 @@
-import React, { useEffect } from 'react';
-import { connect, ConnectedProps } from 'react-redux';
+import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import { sIndex } from './style';
 
 import Carousel from './Carousel';
-import * as users from '../../store/actions/users';
 
 const Landing = (props: IProps) => {
   const c = sIndex();
-  useEffect(() => {
-    props.fetchUsers();
-  }, []);
 
   return (
     <div>
@@ -22,20 +17,11 @@ const Landing = (props: IProps) => {
           <div>Stuck on the Money-Go-Round?</div>
           <div>Bring your money and budgeting back in balance</div>
         </div>
-        {!!props.users.length &&
-          props.users.map((u, i) => <div key={i}>{u.email}</div>)}
       </Container>
     </div>
   );
 };
 
-const mapState = (state) => ({
-  users: state.users.all.data
-});
-const mapDispatch = { ...users };
-const connector = connect(mapState, mapDispatch);
-type PropsFromRedux = ConnectedProps<typeof connector>;
+interface IProps extends RouteComponentProps {}
 
-interface IProps extends PropsFromRedux, RouteComponentProps {}
-
-export default connector(Landing);
+export default Landing;
