aeb3fcc412b916e26be46d96869cafc70bd183cc
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-08-06 14:08:33 +1200

    add in list items for footer items component

--- client/components/Footer/FooterItems.tsx
+++ client/components/Footer/FooterItems.tsx
@@ -9,15 +9,35 @@ const FooterItems = () => {
     <div className={f.conetentContainer1}>
       <div>
         ABOUT
-    </div>
+        <div>
+          <p>About Us</p>
+          <p>Help</p>
+        </div>
+      </div>
       <div>
         CONTACT US
+        <div>
+          <p>Matt Uy</p>
+          <p>Jimmi Meredith</p>
+          <p>Chris Alba</p>
+          <p>Raymond Leung</p>
+          <p>Lucas Santana</p>
+          <p>David Dumolo</p>
+        </div>
       </div>
       <div>
         COURSE
+        <div>
+          <p>Enspiral Dev Academy</p>
+        </div>
     </div>
       <div>
         AWARDS
+        <div>
+          <p>Best Budgeting App 2020</p>
+          <p>Most Innovative Team 2020</p>
+          <p>"Maximum Big Don Energy!" - Don Smith</p>
+        </div>
     </div>
     </div>
   )
