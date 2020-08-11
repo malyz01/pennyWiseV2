e0942288d0c43be0bceddec029f7e7ecf25de67d
malyz <mattuy001@gmail.com>
2020-08-11 16:33:24 +1200

    ADDED: AuthModal component

---
+++ client/containers/AuthModal/Modal.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { sModal } from './styles';
+
+import Fade from '../../components/Fade';
+
+const AuthModal = () => {
+  const classes = sModal();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        react-spring
+      </button>
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="spring-modal-title">Spring modal</h2>
+            <p id="spring-modal-description">react-spring animates me.</p>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
+
+export default AuthModal;
