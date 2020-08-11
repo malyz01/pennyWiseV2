e8f2bd49f3658ae3bf748298556223353b5fdf30
malyz <mattuy001@gmail.com>
2020-08-11 17:40:27 +1200

    UPDATE: paper style

--- client/containers/AuthModal/styles.ts
+++ client/containers/AuthModal/styles.ts
@@ -9,9 +9,8 @@ export const sModal = makeStyles((theme: Theme) =>
     },
     paper: {
       backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3)
+      padding: theme.spacing(2, 4, 3),
+      minHeight: 500
     }
   })
 );
