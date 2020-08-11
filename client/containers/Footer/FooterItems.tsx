1c7caa85591fc5afa68cb792a62a4b45af7d8927
malyz <mattuy001@gmail.com>
2020-08-11 15:54:49 +1200

    UPDATE: style - added strong and breakpoints

--- client/containers/Footer/FooterItems.tsx
+++ client/containers/Footer/FooterItems.tsx
@@ -9,28 +9,36 @@ const FooterItems = () => {
   return (
     <div className={c.mainContainer}>
       <Logo />
-      <div>
-        <p>ABOUT</p>
+      <div className={c.footerItemContainer}>
+        <p>
+          <strong>ABOUT</strong>
+        </p>
         <div>About Us</div>
         <div>Help</div>
       </div>
-      <div>
-        <p>CONTACT US</p>
+      <div className={c.footerItemContainer}>
+        <p>
+          <strong>CONTACT US</strong>
+        </p>
         <div>Matt Uy</div>
         <div>Jimmi Meredith</div>
       </div>
-      <div>
-        <p>COURSE</p>
+      <div className={c.footerItemContainer}>
+        <p>
+          <strong>COURSE</strong>
+        </p>
         <div>Enspiral Dev Academy</div>
       </div>
-      <div className={c.awards}>
-        <p>AWARDS</p>
+      <div className={c.footerItemContainer}>
+        <p>
+          <strong>AWARDS</strong>
+        </p>
         <div>Best Budgeting App 2020</div>
         <div>Most Innovative Team 2020</div>
         <div>"Maximum Big Don Energy!" - Don Smith</div>
       </div>
     </div>
-  )
+  );
 };
 
 export default FooterItems;
