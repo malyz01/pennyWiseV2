0673e622456d1f820ac1a51c6deda27d2e948b27
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-08-11 16:21:05 +1200

    create Style sheet for Profile component

---
+++ client/containers/_Profile/style.ts
@@ -0,0 +1,9 @@
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
+export const sIndex = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      textAlign: 'center'
+    }
+  })
+);
