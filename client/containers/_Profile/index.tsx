fba42f73b7fa2cff70513ec532ea414aa218cd3e
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-08-11 16:23:33 +1200

    Import Profile styling into Profile componet

--- client/containers/_Profile/index.tsx
+++ client/containers/_Profile/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 
+import { sIndex } from './style'
+
 const index = () => {
+  const c = sIndex();
+
   return (
     <div className={c.mainContainer}>
       <Container>
