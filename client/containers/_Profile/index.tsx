303c7d353495a0fcda4486e07d7826629e32af8e
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-08-11 16:06:02 +1200

    Create Profile component

---
+++ client/containers/_Profile/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+const index = () => {
+  return (
+    <div className={c.mainContainer}>
+      <Container>
+
+      </Container>
+    </div>
+  );
+};
+
+export default index;
