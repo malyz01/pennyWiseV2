e0768afd7db1fca89ca9a5334f044b30c40172b2
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-08-12 13:37:29 +1200

    Import CardTop & CardBottom into ProfileCard Component

--- client/containers/_Profile/ProfileCard.tsx
+++ client/containers/_Profile/ProfileCard.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { sProfileCard } from './styles';
+import CardTop from './CardTop';
+import CardBottom from './CardBottom';
 
 const ProfileCard = () => {
   const c = sProfileCard();
 
   return (
     <div className={c.mainContainer}>
-      PROFILE CARD
+      <CardTop />
+      <CardBottom />
     </div>
   )
 };
