e5ed963bbbe17ba8c0e93d2af153f2785c0aad62
malyz <mattuy001@gmail.com>
2020-08-12 19:16:24 +1200

    ADDED: main modal component

---
+++ client/containers/Modal/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Dialog from '@material-ui/core/Dialog';
+import Collapse from '@material-ui/core/Collapse';
+
+import Login from './Login';
+import * as modalActions from '../../store/actions/modal';
+
+const AuthModal = (props) => {
+  const { name, open, setModal } = props;
+  const handleClose = () => {};
+
+  return (
+    <Dialog
+      open={props.open}
+      TransitionComponent={Collapse}
+      keepMounted
+      onClose={handleClose}
+      aria-labelledby="Modal"
+    >
+      {name === 'login' && <Login />}
+    </Dialog>
+  );
+};
+
+const mapState = (state) => ({
+  name: state.modal.name,
+  open: state.modal.open
+});
+const mapProps = { ...modalActions };
+
+export default connect(mapState, mapProps)(AuthModal);
