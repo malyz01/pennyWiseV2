f5e122c2dcf168c8e528bdfc62277b7405d7857a
malyz <mattuy001@gmail.com>
2020-08-13 14:51:14 +1200

    ADDED: styles for login component

--- client/containers/Modal/styles.ts
+++ client/containers/Modal/styles.ts
@@ -1,16 +1,10 @@
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 
-export const sModal = makeStyles((theme: Theme) =>
+export const sLogin = makeStyles((theme: Theme) =>
   createStyles({
-    modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    paper: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(2, 4, 3),
-      minHeight: 500
+    mainContainer: {
+      minHeight: 400,
+      minWidth: 400
     }
   })
 );
