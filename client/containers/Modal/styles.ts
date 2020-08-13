a802267dd4ac14e848396c7bf2e96b23907d8dcf
malyz <mattuy001@gmail.com>
2020-08-13 17:48:46 +1200

    UPDATE: login component styles

--- client/containers/Modal/styles.ts
+++ client/containers/Modal/styles.ts
@@ -3,8 +3,18 @@ import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 export const sLogin = makeStyles((theme: Theme) =>
   createStyles({
     mainContainer: {
-      minHeight: 400,
-      minWidth: 400
+      minHeight: 100,
+      minWidth: 200,
+      padding: '2em 2em 2.5em',
+      background: theme.palette.neutral.main
+    },
+    headerContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    icon: {
+      fontSize: 50
     }
   })
 );
