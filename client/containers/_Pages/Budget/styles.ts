3fe2f52249d548571ac0f5079ce31dc5c4133030
malyz <mattuy001@gmail.com>
2020-08-13 13:54:19 +1200

    ADDED: mainContainer style

---
+++ client/containers/_Pages/Budget/styles.ts
@@ -0,0 +1,10 @@
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
+export const sIndex = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      minHeight: 600,
+      textAlign: 'center'
+    }
+  })
+);
