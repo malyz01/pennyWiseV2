2429b018b1664abe08d250d39c9b5b5a7ea6a9d4
malyz <mattuy001@gmail.com>
2020-09-21 20:46:20 +1200

    Moving App to root

---
+++ client/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Image from 'material-ui-image';
+
+import Page from './pages';
+import Nav from './components/layout/Nav';
+import Footer from './components/layout/Footer';
+import useStyle from './appStyles';
+
+import Modal from './containers/Modal';
+
+const App = () => {
+  const c = useStyle();
+
+  return (
+    <Router>
+      <Modal />
+      <Nav />
+      <div className={c.mainContainer}>
+        <Switch>
+          <Route exact path="/" component={Page.Home} />
+        </Switch>
+        <Footer />
+        <div className={c.fern}>
+          <Image
+            aspectRatio={1 / 1.25}
+            color="none"
+            src="/assets/images/landing/fern.png"
+            alt="Fern"
+          />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
