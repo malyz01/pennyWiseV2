826241ac088411b71dd235697aa4bec23f88c02e
malyz <mattuy001@gmail.com>
2020-10-05 16:19:48 +1300

    Added styles

--- client/pages/styles/profile.ts
+++ client/pages/styles/profile.ts
@@ -5,7 +5,20 @@ export default makeStyles((theme: Theme) =>
     mainContainer: {
       textAlign: 'center',
       minHeight: 600,
-      paddingBottom: '5em'
+      maxWidth: 900,
+      margin: '0 auto'
+    },
+    paper: {
+      padding: '2rem 0',
+      borderRadius: '20px'
+    },
+    container: {
+      minWidth: '200px',
+      maxWidth: '300px',
+      width: '100%'
+    },
+    image: {
+      width: '10em'
     }
   })
 );
