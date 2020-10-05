4d76b17c7c44e841454eeb3c8872463d0498086a
malyz <mattuy001@gmail.com>
2020-10-05 19:56:38 +1300

    Created styles for expense

---
+++ client/pages/styles/expense.ts
@@ -0,0 +1,10 @@
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      minHeight: 600,
+      textAlign: 'center'
+    }
+  })
+);
